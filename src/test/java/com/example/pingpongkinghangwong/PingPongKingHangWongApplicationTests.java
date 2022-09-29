d601acbbb9e24457ef84a382220d0df6b0095321
KingGonW <wong.kinghang1997@gmail.com>
2022-09-29 16:10:57 +0100

    First

---
+++ src/test/java/com/example/pingpongkinghangwong/PingPongKingHangWongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pingpongkinghangwong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingPongKingHangWongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
