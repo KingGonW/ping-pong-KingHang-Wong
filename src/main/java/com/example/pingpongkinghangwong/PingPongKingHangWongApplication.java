0ce941295745016fac7cfe6c7701452be21f900a
KingGonW <wong.kinghang1997@gmail.com>
2022-09-29 16:33:40 +0100

    Second

--- src/main/java/com/example/pingpongkinghangwong/PingPongKingHangWongApplication.java
+++ src/main/java/com/example/pingpongkinghangwong/PingPongKingHangWongApplication.java
@@ -2,12 +2,21 @@ package com.example.pingpongkinghangwong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PingPongKingHangWongApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PingPongKingHangWongApplication.class, args);
+	}
+
+	@GetMapping("/ping")
+	public String PingPong(){
+		return String.format("pong");
 	}
 
 }
